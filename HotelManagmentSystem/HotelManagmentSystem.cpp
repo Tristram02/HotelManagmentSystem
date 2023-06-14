130132bb400cd421b3cd711fd52c24b97daafa44
Tristram02 <wldoekpl@gmail.com>
2023-06-14 20:48:06 +0200

    Init commit

---
+++ HotelManagmentSystem/HotelManagmentSystem.cpp
@@ -0,0 +1,12 @@
+﻿// HotelManagmentSystem.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "HotelManagmentSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
